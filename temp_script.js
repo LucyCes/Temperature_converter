7e5caf1f6dc372f50fe749591a450ed9b8a96b3b
LucyCes <cespedesl17@mail.wlu.edu>
2017-03-16 09:35:09 -0400

    msg variable

--- temp_script.js
+++ temp_script.js
@@ -1,16 +1,16 @@
 var temperatureInput = prompt("Enter a temperature in Farenheit, and I will give you its equivalence in Celsius, and the next nine lower temperatures de cinco en cinco because why not");
 
 if (Number(temperatureInput)) {
-    document.getElementById("tempresults").innerHTML = "<table border='1'>";
-    document.getElementById("tempresults").innerHTML = "<tr>";
-    document.getElementById("tempresults").innerHTML = "<td>" + temperatureInput + " F" + "</td>" + "<td>" + (5 / 9 * (temperatureInput - 32)) + " C" + "</td>";
-    document.getElementById("tempresults").innerHTML = "</tr>";
+    var msg = "<table border='1'>";
+    msg = msg + "<tr>";
+    msg = msg + "<td>" + temperatureInput + " F" + "</td>" + "<td>" + (5 / 9 * (temperatureInput - 32)) + " C" + "</td>";
+    msg = msg + "</tr>";
     for (var i = 0; i < 10; i++) {
       console.log(i);
       var temperatureInput = temperatureInput-5;
-      document.getElementById("tempresults").innerHTML = "<tr>";
-      document.getElementById("tempresults").innerHTML = "<td>" + temperatureInput + " F" + "</td>" + "<td>" + (5 / 9 * (temperatureInput - 32)) + " C" + "</td>";
-      document.getElementById("tempresults").innerHTML = "</tr>";
+      msg = msg + "<tr>";
+      msg = msg + "<td>" + temperatureInput + " F" + "</td>" + "<td>" + (5 / 9 * (temperatureInput - 32)) + " C" + "</td>";
+      msg = msg + "</tr>";
     }
     document.getElementById("tempresults").innerHTML = "</table>";
 } else {
